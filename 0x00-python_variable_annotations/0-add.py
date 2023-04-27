862300c635632f318afd91af4614403f1e2f6f47
pelumi <polalekan@gmail.com>
2023-04-27 16:47:31 +0100

    project 00 setup

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+"""
+0. Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    A type-annotated function add that takes a float a and a float b
+    as arguments and returns their sum as a float.
+    """
+
+    return a + b
