841a4e8668e1be3f539b10a963ef19ae41d5bb1e
pelumi <polalekan@gmail.com>
2023-04-27 17:03:07 +0100

    task 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,7 +6,7 @@
 
 def add(a: float, b: float) -> float:
     """
-    A type-annotated function add that takes a float a and a float b
+    A type-annotated function that takes a float a and a float b
     as arguments and returns their sum as a float.
     """
 
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+"""
+1. Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A type-annotated function that takes a string str1 and a string str2
+    as arguments and returns a concatenated string
+    """
+
+    return str1 + str2
