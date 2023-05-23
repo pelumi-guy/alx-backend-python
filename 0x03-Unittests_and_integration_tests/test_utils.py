e28f25381cc3040e3f6aa4e75557569230f2728d
pelumi <polalekan@gmail.com>
2023-05-23 02:12:26 +0100

    03 - task 2 and 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,3 +38,67 @@ class TestAccessNestedMap(TestCase):
         """
 
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    A class that inherits from unittest.TestCase
+    for carrying out unit test on utils.get_json
+    """
+    @parameterized.expand(
+        [
+            ('http://example.com', {'payload': True}),
+            ('http://holberton.io', {'payload': False})
+        ]
+    )
+    def test_get_json(self, url, expected_output):
+        """
+        A method to test that utils.get_json returns the expected result.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = expected_output
+        with patch('requests.get', return_value=mock_response):
+            response = get_json(url)
+
+            self.assertEqual(response, expected_output)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Class for testing the utils.memoize decorator
+    """
+
+    def test_memoize(self):
+        """
+        Method to test utils.memoize
+        """
+
+        class TestClass:
+            """
+            Mock test class for testing
+            """
+
+            def a_method(self):
+                """
+                Mock test object method
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """
+                Mock test object method
+                """
+                return self.a_method()
+
+        test_obj = TestClass()
+
+        with patch.object(test_obj, 'a_method') as mock_method:
+            mock_method.return_value = 42
+
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
